9df5f45f6a942c8eb25da991f2f2a876ce626a6a
Kaan <kyildirim@ziraatteknoloji.com>
2020-12-14 17:03:26 +0300

    - 1. gün eğitim kodları eklendi. - Readme revize edildi.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 iOS Eğitim Döküman ve Kodları
 
 1. Gün Genel Syntax ve Git Eğitimi.
+
+- Git repo oluşturma.
+- Git commit pull push.
+- Readme ekleme.
+- Xcode playground oluşturma.
+- Var let types print
+- if yapısı
+- for yapısı
---
+++ iOSEğitim.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+//var let
+var str = "Hello, playground"
+str = "Hi"
+
+var str_2 = "Merhaba"
+str_2 = ""
+
+//Types
+var integer = 1
+var double: Double = 1.1
+var float = 1.1
+
+//Printing
+print("Integer value is " + "\(integer)")
+print(str)
+
+//Array
+var myArray = ["Apple", "Orange", "Banana"]
+myArray.append("Grape")
+
+print(myArray)
+
+//If
+let controlValue = 5
+var checkIntegerValue = 5
+
+if checkIntegerValue == controlValue {
+    print("Value == " + "\(controlValue)")
+}
+
+//For
+var forArray = [1, 2, 3, 4, 5]
+
+for ahmet in forArray {
+    print(ahmet)
+}
